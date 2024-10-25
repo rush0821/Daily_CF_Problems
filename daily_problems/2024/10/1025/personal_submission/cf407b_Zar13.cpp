14ceacb47779b364137942f9f61b2c3237db335c
Zar13 <131002306+rush0821@users.noreply.github.com>
2024-10-25 16:53:49 +0800

    20241025

---
+++ daily_problems/2024/10/1025/personal_submission/cf407b_Zar13.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+const int N=1005, mod=1e9+7;
+int n, p[N], f[N];
+int c[N], res, x=1;
+bool vis[N];
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>p[i];
+//    f[1]=1;
+    for(int i=2;i<=n+1;i++){
+//    	f[i]=2*f[i-1]-f[p[i-1]]+2;
+    	f[i]=(2*f[i-1]%mod-f[p[i-1]]+2+mod)%mod;
+	}
+//	cout<<"f1:"<<f[1]<<endl;
+	cout<<f[n+1];
+    return 0;
+}
